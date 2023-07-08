0e9d9f082953c59a101faee97b60579e47298f10
Cronell1800 <otaboyev.d.0518@gmail.com>
2023-07-08 09:41:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Number_guesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Random random = new Random();
+            int guessedNumber = random.Next(1, 100);
+
+            Console.WriteLine("Can you guess the number i'm thinking?");
+
+            do
+            {
+                Console.WriteLine("Enter guessed number: ");
+                String number = Console.ReadLine();
+                int enteredNumber = int .Parse(number);
+                counter++;
+
+                if (enteredNumber == guessedNumber)
+                {
+                    break;
+                } else if (enteredNumber < guessedNumber)
+                {
+                    Console.WriteLine("My guessed number is bigger");
+                } else if (enteredNumber > guessedNumber)
+                {
+                    Console.WriteLine("My guessed number is smaller");
+                }
+
+
+            } while (counter < guessedNumber);
+
+            Console.WriteLine("You have found the number i guessed in " + counter + " turns");
+        }
+    }
+}
